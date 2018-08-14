83a3798e1fd41a6b9b99d73b1f166316229cab38
Alireza Andersen <alireza.andersen@gmail.com>
2018-08-14 10:59:16 -0600

    have access to fetch_weather

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,4 +1,16 @@
 module WeatherHelper
 
 
+  WEATHER = ['windy', 'cloudy', 'mostly cloudy', 'partly cloudy', 'clear', 'sunny', 'fair', 'hot']
+
+  def self.tubing_outcomes
+    @results = fetch_weather
+  end
+
+private
+
+  def self.fetch_weather
+    JSON.parse(RestClient.get("https://boulder-tubing-api.herokuapp.com/conditions"))
+  end
+
 end
