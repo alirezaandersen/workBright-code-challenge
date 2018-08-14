30303b03edbb937fe7ba7a4606a34707db1bb3d8
Alireza Andersen <alireza.andersen@gmail.com>
2018-08-14 11:06:50 -0600

    conditions met

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -4,13 +4,31 @@ module WeatherHelper
   WEATHER = ['windy', 'cloudy', 'mostly cloudy', 'partly cloudy', 'clear', 'sunny', 'fair', 'hot']
 
   def self.tubing_outcomes
-    @results = fetch_weather
+    if temperature && conditions && river_flow
+      @results = "LETS GO TUBING"
+    else
+      @results = "Weather ain't that great"
+    end
   end
 
-private
+  private
 
   def self.fetch_weather
     JSON.parse(RestClient.get("https://boulder-tubing-api.herokuapp.com/conditions"))
+  end
+
+  def self.temperature
+    fetch_weather["temperature_f"].to_i > 85
+  end
+
+  def self.conditions
+    cond = fetch_weather["weather_condition"]
+    WEATHER.any? { |word| cond.include?(word) }
+  end
+
+  def self.river_flow
+    cfs = fetch_weather["flow_rate_cfs"].to_i
+    cfs > 40 && cfs < 300
   end
 
 end
